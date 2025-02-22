298348f2b7d5e88884a3ec0ec8379ef6acbe1699
DPBhagya99 <piyumibhagya1007@gmail.com>
2025-02-22 12:25:23 +0530

    new changes

---
+++ data/index.ts
@@ -0,0 +1,358 @@
+
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "About Me ",
+      description: "Hi, I’m Piyumi Bhagya, A pending graduate in Computer Science, Statistics & Mathematics with a strong passion for Software Quality Assurance. A detail-oriented and analytical individual with a problem-solving mindset, eager to ensure software reliability and efficiency through thorough testing and defect management.          \n" +
+          "Currently, I am honing my skills in Quality Assurance (QA), and Full-Stack Development. My technical expertise spans multiple programming languages, libraries, and frameworks:\n" +
+          "\n" +
+          "Languages: Java, Python, JavaScript, C\n" +
+          "Frontend Development: React.js, Next.js, jQuery\n" +
+          "Backend Development: Node.js, Express.js\n" +
+          "Databases: MongoDB, SQL, MySQL\n" +
+          "Testing Technologies: Selenium, TestNG, JMeter, Postman\n" +
+          "I am constantly learning and applying my knowledge in real-world projects and academic endeavors, striving to bridge theoretical understanding with practical application. I am particularly passionate about leveraging modern tools and frameworks to build scalable, high-performance, and user-friendly applications.\n" +
+          "\n" +
+          "With a strong foundation in Quality Assurance and software engineering principles and a growing repertoire of technologies, I aspire to excel in roles that challenge me to innovate, collaborate, and contribute to impactful projects.",
+      className: "lg:col-span-4 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-center flex-center flex font-bold white mb:100px md:100px",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    // {
+    //   id: 5,
+    //   title: "Currently building a JS Animation library",
+    //   description: "The Inside Scoop",
+    //   className: "md:col-span-3 md:row-span-2",
+    //   imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    //   titleClassName: "justify-center md:justify-start lg:justify-center",
+    //   img: "/b5.svg",
+    //   spareImg: "/grid.svg",
+    // },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Trip and Travel Planning Web Project ",
+      des: " Developed a comprehensive web-based trip and travel planning system tailored for travelers visiting Sri Lanka, contributing to the overall development of the platform. Specifically designed and implemented the weather forecasting system to provide real-time weather updates for travel destinations and a special event reminder system to notify travelers of important events during their trips.using MERN stack.",
+      img: "/Trip.png",
+      iconLists: ["/react.svg", "/mongodb.svg", "/nodejs.svg", "/express.svg"],
+      link: "https://github.com/DPBhagya99/Trip-and-Travel-Planning",
+    },
+    {
+      id: 2,
+      title: "NewsLetter App",
+      des: "NewsLetter signup App using Mailchimp.",
+      img: "/p2.svg",
+      iconLists: ["/js.svg", "/nodejs.svg", "/express.svg"],
+      link: "https://github.com/DPBhagya99/NewsletterApp",
+    },
+    // {
+    //   id: 3,
+    //   title: "Currency-Value-App",
+    //   des: "A currency converter app is a digital tool designed to provide users with real-time exchange rates between different currencies. These apps are particularly useful for travelers, international businesses, and individuals dealing with multiple currencies, as they offer quick and accurate conversions on the go..",
+    //   img: "/p3.svg",
+    //   iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    //   link: "https://github.com/HarshaKTM/currency_value_app",
+    // },
+    // {
+    //   id: 4,
+    //   title: "EV Charging Mobile Application",
+    //   des: "The EV Charging Mobile Application is a user-friendly app designed to help electric vehicle (EV) owners find, book, and manage charging stations on the go. It offers real-time station availability, navigation, and secure payments, ensuring a smooth charging experience...",
+    //   img: "/p4.svg",
+    //   iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    //   link: "https://github.com/Sandeepa-Sadewaka/orel_ev_mobile_app",
+    // },
+    // {
+    //   id: 4,
+    //   title: "EV Charging Web Application",
+    //   des: "The EV Charging Web Application is a full-stack platform designed to help electric vehicle (EV) users locate, book, and manage charging stations efficiently. It provides real-time station availability, pricing, and navigation while allowing station owners to monitor usage and payments..",
+    //   img: "/p4.svg",
+    //   iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    //   link: "https://github.com/GihanJY/orel_ev_web_app",
+    // },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Collaborating with Jack was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Jack's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Jack is the ideal partner.",
+      name: "John Smith",
+      title: "Director of Kodec Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Jane was an absolute pleasure. Her professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Jane's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Jane is the ideal partner.",
+      name: "John Smith",
+      title: "Director of Kodec Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Jack was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Jack's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Jack is the ideal partner.",
+      name: "John Smith",
+      title: "Director of Kodec Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Jane was an absolute pleasure. Her professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Jane's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Jane is the ideal partner.",
+      name: "John Smith",
+      title: "Director of Kodec Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "John Smith",
+      title: "Director of Kodec Technologies",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "React",
+      img: "/react.svg",
+      nameImg: "/react.svg",
+    },
+    {
+      id: 2,
+      name: "java",
+      img: "/java.svg",
+      nameImg: "/java.svg",
+    },
+    // {
+    //   id: 3,
+    //   name: "HOSTINGER",
+    //   img: "/host.svg",
+    //   nameImg: "/host.svg",
+    // },
+    {
+      id: 3,
+      name: "jenkins",
+      img: "/jenkins.svg",
+      nameImg: "/jenkins.svg",
+    },
+    {
+      id: 4,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dock.svg",
+    },
+    // {
+    //   id: 6,
+    //   name: "flutter",
+    //   img: "/flutter.svg",
+    //   nameImg: "/flutter.svg",
+    // },
+    //   {
+    //   id: 7,
+    //   name: "gitlab",
+    //   img: "/gitlab.svg",
+    //   nameImg: "/gitlab.svg",
+    // },
+    //   {
+    //   id: 8,
+    //   name: "aws",
+    //   img: "/aws.svg",
+    //   nameImg: "/aws.svg",
+    // },
+      {
+      id: 5,
+      name: "mongodb",
+      img: "/mongodb.svg",
+      nameImg: "/mongodb.svg",
+    },
+      {
+      id: 6,
+      name: "nodejs",
+      img: "/nodejs.svg",
+      nameImg: "/nodejs.svg",
+    },
+    //   {
+    //   id: 11,
+    //   name: "kubernetes",
+    //   img: "/kubernetes.svg",
+    //   nameImg: "/kubernetes.svg",
+    // },
+      // {
+      // id: 12,
+      // name: "typescript",
+      // img: "/ts.svg",
+      // nameImg: "/ts.svg",
+    // },
+      {
+      id: 7,
+      name: "sql",
+      img: "/mysql.svg",
+      nameImg: "/mysql.svg",
+    },
+    //   {
+    //   id: 14,
+    //   name: "azure",
+    //   img: "/azure.svg",
+    //   nameImg: "/azure.svg",
+    // },
+      {
+      id: 8,
+      name: "nextjs",
+      img: "/next.svg",
+      nameImg: "/next.svg",
+    },
+      {
+      id: 9,
+      name: "tailwindcss",
+      img: "/tail.svg",
+      nameImg: "/tail.svg",
+    },
+      {
+      id: 10,
+      name: "selenium",
+      img: "/selenium.svg",
+      nameImg: "/selenium.svg",
+    },
+      {
+      id: 11,
+      name: "git",
+      img: "/git.svg",
+      nameImg: "/git.svg",
+    },
+      {
+      id: 12,
+      name: "python",
+      img: "/python.svg",
+      nameImg: "/python.svg",
+    },
+      {
+      id: 13,
+      name: "TestNG",
+      img: "/TestNG.svg",
+      nameImg: "/TestNG.svg",
+    },
+      {
+      id: 14,
+      name: "jquery",
+      img: "/jquery.svg",
+      nameImg: "/jquery.svg",
+    },
+      {
+      id: 15,
+      name: "JMeter",
+      img: "/JMeter.svg",
+      nameImg: "/JMeter.svg",
+    },
+      {
+      id: 16,
+      name: "postman",
+      img: "/postman.svg",
+      nameImg: "/postman.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "WordPress Developer",
+      desc: "This project aims to develop a dental medical centre website using WordPress, creating a responsive and user-friendly platform that effectively showcased services and facilitated easy appointment scheduling.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+      link: "../components/details/ReactDetails.tsx",
+    },
+    {
+      id: 2,
+      title: "React and Advanced React Certificate Course on Scrimba",
+      desc: "This course is designed to help you master React and advance your skills to tackle complex front-end development tasks. The course covers foundational React concepts such as components, state management, hooks, and routing. As you progress, you'll dive into advanced React topics like performance optimization, server-side rendering (SSR), testing, and using popular React frameworks such as Next.js. By the end of the course, you'll have a comprehensive understanding of React and be equipped with the skills to build scalable and high-performance web applications.\n" +
+          "\n " +
+          "Key Topics:\n" +
+          "\n" +
+          "React Basics: Components, JSX, Props, State, Event Handling\n" +
+          "React Hooks: useState, useEffect, useContext, useReducer, Custom Hooks\n" +
+          "React Router: Navigating between pages, dynamic routes, and route parameters\n" +
+          "State Management: Context API, Redux, MobX\n" +
+          "Advanced React Patterns: Compound Components, Higher-Order Components (HOC), Render Props\n" +
+          "Performance Optimization: Code splitting, lazy loading, memoization\n" +
+          "Server-Side Rendering (SSR): Introduction to SSR, Next.js\n" +
+          "React Testing: Writing tests for React components using Jest and React Testing Library\n" +
+          "TypeScript with React: Integrating TypeScript into React projects for type safety\n",
+          // "Deploying React Apps: Hosting React apps using services like Netlify, Vercel, and AWS",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    // {
+    //   id: 3,
+    //   title: "DevOps Engineer",
+    //   desc: "DevOps Engineer for Deloping and monitoring product ",
+    //   className: "md:col-span-2", // change to md:col-span-2
+    //   thumbnail: "/exp3.svg",
+    // },
+    // {
+    //   id: 4,
+    //   title: "Frontend Developer",
+    //   desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    //   className: "md:col-span-2",
+    //   thumbnail: "/exp4.svg",
+    // },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/github.svg",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
